c0e02db4ec350f8e1dfad68ee93c95cce2a1fde7
tnw5222 <143755710+tnw5222@users.noreply.github.com>
2023-08-31 17:45:02 -0400

    Add files via upload

---
+++ Quiz1CMake.cpp
@@ -0,0 +1,12 @@
+﻿// Quiz1CMake.cpp : Defines the entry point for the application.
+//
+
+#include "Quiz1CMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Tyson Warner" << endl;
+	return 0;
+}
